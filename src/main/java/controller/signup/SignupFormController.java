c6031f5bbfb5337a6a9adebb2b8336e74efc4b0c
Yasith <141606159+yasith-1@users.noreply.github.com>
2025-07-07 09:51:36 +0530

    refactor: improve user registration flow and enhance UI elements in signup and login screens

--- src/main/java/controller/signup/SignupFormController.java
+++ src/main/java/controller/signup/SignupFormController.java
@@ -10,11 +10,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.User;
 import org.controlsfx.control.Notifications;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -38,49 +36,49 @@ public class SignupFormController implements Initializable {
     @FXML
     void RegisterOnActionButton(ActionEvent event) {
         if (usernameField.getText().isEmpty()) {
-            Notifications.create().title("Warning").text("Enter an username..").position(Pos.BOTTOM_RIGHT).showInformation();
+            Notifications.create().title("Warning").text("Enter a username.").position(Pos.BOTTOM_RIGHT).showInformation();
             return;
         } else if (passwordField.getText().isEmpty()) {
-            Notifications.create().title("Warning").text("Enter a Strong password..").position(Pos.BOTTOM_RIGHT).showInformation();
+            Notifications.create().title("Warning").text("Enter a strong password.").position(Pos.BOTTOM_RIGHT).showInformation();
             return;
         } else {
             try {
-                Boolean isExist = SignupController.getInstance().notifyAlreadyUserExist(usernameField.getText());
+                boolean isExist = SignupController.getInstance().notifyAlreadyUserExist(usernameField.getText());
                 if (isExist) {
-//                    user exist
-                    Notifications.create().title("Warning").text("User Already Registerd..").position(Pos.BOTTOM_RIGHT).showInformation();
+                    Notifications.create().title("Warning").text("User already registered.").position(Pos.BOTTOM_RIGHT).showInformation();
                 } else {
-//                     user not exist
-                    User user = new User(idField.getText(),usernameField.getText(),passwordField.getText());
-                    Boolean isAdded = SignupController.getInstance().addUser(user);
-                    if (isAdded){
-                        Notifications.create().title("Sucess").text("User Registerd Sucessfully !").position(Pos.BOTTOM_RIGHT).showInformation();
-                    }else {
-                        Notifications.create().title("Warning").text("User Registration fail , try again !").position(Pos.BOTTOM_RIGHT).showInformation();
-                        return;
+                    User user = new User(idField.getText(), usernameField.getText(), passwordField.getText());
+                    boolean isAdded = SignupController.getInstance().addUser(user);
+                    if (isAdded) {
+                        Notifications.create().title("Success").text("User registered successfully!").position(Pos.BOTTOM_RIGHT).showInformation();
+
+                        // Go to login window-------------------------------------------------------------------------
+                        gotoLoginWindowOnActionBtn( event);
+                    } else {
+                        Notifications.create().title("Error").text("User registration failed. Try again.").position(Pos.BOTTOM_RIGHT).showError();
                     }
                 }
             } catch (Exception e) {
-                throw new RuntimeException(e);
+                Notifications.create().title("Exception").text("Something went wrong: " + e.getMessage()).position(Pos.BOTTOM_RIGHT).showError();
+                e.printStackTrace();
             }
         }
-
     }
 
-
-    public void backToLoginText(MouseEvent mouseEvent) throws IOException {
-        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
-        stage.close();
-
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/user-login.fxml"))));
-        stage.setTitle("Login");
-        stage.getIcons().add(new Image("/images/icon.png"));
-        stage.show();
-    }
-
-    private void setUserId(){
+    private void setUserId() {
         String userId = SignupController.getInstance().generateuserId();
         idField.setText(userId);
     }
 
+    @FXML
+    public void gotoLoginWindowOnActionBtn(ActionEvent event) throws IOException {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("/view/user-login.fxml")));
+        stage.setScene(scene);
+        stage.setTitle("Login");
+        stage.getIcons().add(new Image("/images/icon.png"));
+        stage.show();
+    }
 }
